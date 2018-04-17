c1bc57ec798b53026df9f56c79d7dd0f159309f6
Austin Heyne <aheyne@ccri.com>
2018-04-17 10:06:17 -0400

    Version gm.6 bump
    Signed-off-by: Austin Heyne <aheyne@ccri.com>

--- geopyspark-backend/project/Version.scala
+++ geopyspark-backend/project/Version.scala
@@ -1,5 +1,5 @@
 object Version {
-  val geopyspark = "0.3.0.gm.5"
+  val geopyspark = "0.3.0.gm.6"
   val geotrellis = "2.0.0-SNAPSHOT"
   val geomesa    = "2.0.0-SNAPSHOT"
   val scala      = "2.11.11"
